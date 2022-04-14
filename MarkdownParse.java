b0b10364aea0c2ca2a98ff513c50c8c775ea9aee
Nicholas Quach <nquach1515@gmail.com>
2022-04-14 12:43:34 -0700

    updated MarkdownParse.java to handle bug

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,9 +17,13 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (openBracket == -1 || closeBracket == -1 || openParen == -1 || 
+                closeParen == -1) {
+                    break;
+                }
             currentIndex = closeParen + 1;
         }
-
+ 
         return toReturn;
     }
 
