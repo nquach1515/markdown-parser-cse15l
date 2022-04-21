ad58d279dd65d36bc16b5dba437531a2c2310b1c
Nicholas Quach <nquach1515@gmail.com>
2022-04-21 12:42:03 -0700

    added getLinksTest method to test

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
 // two lines above are to import JUnit
 
 public class MarkdownParseTest {
@@ -8,5 +11,14 @@ public class MarkdownParseTest {
     public void addition() {
         assertEquals(2, 1+1); 
         // checks that the arguments are equal
+    }
+
+    @Test
+    public void getLinksTest() throws IOException {
+        Path fileName = Path.of("test1.md");
+        String content = Files.readString(fileName);
+        MarkdownParse test = new MarkdownParse();
+        List expected = List.of("https://something.com", "some-thing.html");
+        assertEquals(test.getLinks(content), expected);
     }
 }
