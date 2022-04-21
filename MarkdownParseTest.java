5d6cd18f776f31b03a36712427fbee457770c5ab
Nicholas Quach <nquach1515@gmail.com>
2022-04-21 12:20:44 -0700

    created junit tests

---
+++ MarkdownParseTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+// two lines above are to import JUnit
+
+public class MarkdownParseTest {
+    
+    @Test // declares that this method is a test
+    public void addition() {
+        assertEquals(2, 1+1); 
+        // checks that the arguments are equal
+    }
+}
